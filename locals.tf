239884b8835201cc4bfec565a514f5e6471c8f4e
Andrés Vásquez <andres.vasquez1@udea.edu.co>
2022-08-07 10:45:44 -0500

    feat: Create locals

--- locals.tf
+++ locals.tf
@@ -1,9 +1,11 @@
 locals {
   aws_region = "us-east-1"
-  prefix     = "Terraform-ECS-Demo"
+  prefix     = var.project
   common_tags = {
-    Project   = local.prefix
-    ManagedBy = "Terraform"
+    "Name"          = var.project
+    "Project"       = var.project
+    "Environment"   = var.environment
+    "ManagedBy"     = "Terraform"
   }
   vpc_cidr = var.vpc_cidr
 }
